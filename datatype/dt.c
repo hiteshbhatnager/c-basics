22fe952accb4d87432c1e3da9c0ffa71a18f604f
hitesh <bhatnagerhitesh@gmail.com>
2026-09-08 19:27:36 +0530

    dt

--- datatype/dt.c
+++ datatype/dt.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
 int main()
+{
+    int number = 1;        // store integer
+    float dec = 2.4344994; // store single prcise num
+    double decDec = 5422.359292;
+    char ch = 'a';
+
+    printf("integer = %d\n", number);
+    printf("float = %f\n", dec);
+    printf("dobule = %lf\n", decDec);
+    printf("Char = %c\n", ch)
+}
