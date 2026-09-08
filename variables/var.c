eadb761eb5f419a93797cc71828ad1b6b4457b65
hitesh <bhatnagerhitesh@gmail.com>
2026-09-08 18:48:43 +0530

    variables

---
+++ variables/var.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// c language is case sensitive
+
+int main()
+{
+    int var;  // variable declaration ,variable name is call identifier and start with alphabat and _
+    var = 14; // variable initialization
+
+    int firstNum = 20; // camel case
+
+    printf("var = %d", var);
+    return 0;
+}
