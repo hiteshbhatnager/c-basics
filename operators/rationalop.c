9498e1654df23afb84ba97735ef0e29f5b714aab
hitesh <bhatnagerhitesh@gmail.com>
2026-09-11 22:08:37 +0530

    rational operator

---
+++ operators/rationalop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 50, b = 20;
+    printf("%d\n", (a == b));
+    printf("%d\n", (a < b));
+    printf("%d\n", (a > b));
+    printf("%d\n", (a != a));
+}
