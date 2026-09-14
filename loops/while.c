32bfdb8c683245bdf172dbd0c1e22e04a41c919b
hitesh <bhatnagerhitesh@gmail.com>
2026-09-14 19:19:57 +0530

    loops

--- loops/while.c
+++ loops/while.c
@@ -11,15 +11,26 @@ int main()
 
     // sum 382292 with while loop
 
-    int digit = 382292;
-    int i;
-    double sum;
-    while (digit != 0)
+    // int digit = 382292;
+    // int i;
+    // double sum;
+    // while (digit != 0)
+    // {
+    //     i = digit % 10;
+    //     printf("last digit : %d\n", i);
+    //     digit = digit / 10;
+    //     sum = sum + i;
+    // }
+    // printf("Sum of digits: %lf\n", sum);
+
+    // reverse number
+    int num = 23456;
+    double rev;
+    while (num != 0)
     {
-        i = digit % 10;
-        printf("last digit : %d\n", i);
-        digit = digit / 10;
-        sum = sum + i;
+        int i = num % 10;
+        num /= 10;
+        rev = rev * 10 + i;
     }
-    printf("Sum of digits: %lf\n", sum);
+    printf("%lf", rev);
 }
