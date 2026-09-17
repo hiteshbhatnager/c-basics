2ad890ccbd5a69cf05038ad1d22d463c4b626a45
hitesh <bhatnagerhitesh@gmail.com>
2026-09-17 18:50:11 +0530

    x pattren

---
+++ nestedloop/xpatt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        for (int a = 1; a <= 6 - i; a++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
